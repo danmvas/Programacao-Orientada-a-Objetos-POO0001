5283e8f691a9451b823fb0499fca922d61c2f733
Daniella Vasconcellos <danivasco@msn.com>
2021-06-06 23:49:04 -0300

    aaa

--- 2021-1/Listas de Exercícios/lista1/Ex6.java
+++ 2021-1/Listas de Exercícios/lista1/Ex6.java
@@ -1,31 +1,41 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Ex6 {
 
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Insira a primeira String:");
+        System.out.println("Insira a primeira String: ");
         String string1 = leitor.nextLine();
-        String string1min = string1.toLowerCase();
+        String string1min = string1.toLowerCase().replace(" ", "");
 
-        System.out.println("Insira a segunda String");
+        System.out.println("Insira a segunda String: ");
         String string2 = leitor.nextLine();
-        String string2min = string2.toLowerCase();
+        String string2min = string2.toLowerCase().replace(" ", "");
 
-        Character[] letras = new Character[26];
+        String letras = "";
+        int cont = 0;
 
-        for(int i=0; i < string1.length(); i++){
-            char ci = string1.charAt(i);
+        for(int i=0; i < string1min.length(); i++){
+            Character ci = string1min.charAt(i);
 
-            for(int j = 0; j < string2.lenght(); j++){
-                char cj = string1.charAt(j);
-                if (ci == cj) 
+            if(!letras.contains(ci.toString())){
+                for(int j = 0; j < string2min.length(); j++){
+                char cj = string2min.charAt(j);
+                if (ci == cj){
+                    letras += ci;
+                    cont++;
+                }
             }
         }
+        }
+
+        char[] letrasOrganizadas = letras.toCharArray();
+        Arrays.sort(letrasOrganizadas);
 
         for (int k = 0; k < letras.length(); k++)
-            System.out.println("As letras presentes em ambas as Strings sao: " + letras[k]);
+            System.out.println("As letras presentes em ambas as Strings sao: " + letrasOrganizadas[k]);
 
         leitor.close();
     }
