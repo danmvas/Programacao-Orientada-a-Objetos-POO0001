190d5cef534251d41b74e56774aa87d5bbde51b3
Daniella Vasconcellos <danivasco@msn.com>
2021-06-06 23:27:42 -0300

    harder

--- 2021-1/Listas de Exercícios/lista1/Ex6.java
+++ 2021-1/Listas de Exercícios/lista1/Ex6.java
@@ -7,22 +7,27 @@ public class Ex6 {
 
         System.out.println("Insira a primeira String:");
         String string1 = leitor.nextLine();
+        String string1min = string1.toLowerCase();
 
         System.out.println("Insira a segunda String");
         String string2 = leitor.nextLine();
+        String string2min = string2.toLowerCase();
+
+        Character[] letras = new Character[26];
 
         for(int i=0; i < string1.length(); i++){
-            for(int j = 0; j < string2.lenght(); j ++){
-                if (){
+            char ci = string1.charAt(i);
 
-                }
+            for(int j = 0; j < string2.lenght(); j++){
+                char cj = string1.charAt(j);
+                if (ci == cj) 
             }
         }
 
-        System.out.println("As letras presentes em ambas as Strings sao: " + letras);
+        for (int k = 0; k < letras.length(); k++)
+            System.out.println("As letras presentes em ambas as Strings sao: " + letras[k]);
 
         leitor.close();
     }
-    
-    
+
 }
--- 2021-1/Listas de Exercícios/lista1/Ex7.java
+++ 2021-1/Listas de Exercícios/lista1/Ex7.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
-import java.time.LocalDate;
 import java.util.Calendar;
 
 public class Ex7 {
 
     public static void main(String[] args) {
-        int ano_pc = Calendar.getInstance().get(Calendar.YEAR);
+        Calendar calendar = Calendar.getInstance();
+        int dia_pc = calendar.get(Calendar.DATE+1);
+        int mes_pc = calendar.get(Calendar.MONTH+1);
+        int ano_pc = calendar.get(Calendar.YEAR);
 
         Scanner leitor = new Scanner(System.in);
         System.out.println("Insira a data de nascimento");
@@ -21,25 +23,14 @@ public class Ex7 {
             System.exit(0);
         }
         
-        LocalDate dataAtual = LocalDate.now();
-        LocalDate dataNascimento = LocalDate.of(ano, mes, dia);
-        LocalDate dataFinal = dataAtual - dataNascimento;
+        int contaAnosAnteriores = 365 * (ano_pc - ano - 2);
+        int contaDiasAnoAtual = ((mes_pc-1)*30 + (30 - dia_pc));
+        int contaMesesAnoNascimento = 30 * (12 - mes); 
+        int dias = contaAnosAnteriores + contaDiasAnoAtual + contaMesesAnoNascimento;
 
-        System.out.println("Dias = " + dias);   
+        System.out.println("Voce tem aproximadamente " + dias + " dias de vida, continue assim!");
 
         leitor.close();
     }
     
 }
-
-        // int contaAnosAnteriores = 365 * (ano_pc - ano - 1);
-        // int contaDiasAnoAtual = ((mes_pc-1)*30 + Calendar.getInstance().get(Calendar.));
-
-        // int contaMesesAnoNascimento = 30 * (12 - mes); 
-
-
-        // int dias = contaAnosAnteriores + contaDiasAnoAtual + contaMesesAnoNascimento;
-
-        // System.out.println("Voce tem aproximadamente " + dias + "de vida, continue assim!");
-
-
