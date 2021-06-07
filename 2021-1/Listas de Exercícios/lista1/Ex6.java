7f900b7e18dab4cca542c2b9870b684acee64aae
Daniella Vasconcellos <danivasco@msn.com>
2021-06-06 21:31:59 -0300

    aa

--- 2021-1/Listas de Exercícios/lista1/Ex6.java
+++ 2021-1/Listas de Exercícios/lista1/Ex6.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Ex6 {
 
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Insira a primeira String:");
+        String string1 = leitor.nextLine();
+
+        System.out.println("Insira a segunda String");
+        String string2 = leitor.nextLine();
+
+        for(int i=0; i < string1.length(); i++){
+            for(int j = 0; j < string2.lenght(); j ++){
+                if (){
+
+                }
+            }
+        }
+
+        System.out.println("As letras presentes em ambas as Strings sao: " + letras);
+
+        leitor.close();
+    }
     
     
 }
